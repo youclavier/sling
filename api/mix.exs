f05e8d4f955206ad0e071c8ba1b6626a02d6ee2a
James <james@Jamess-iMac.local>
2018-06-28 18:09:30 +0800

    add_comeonin

--- api/mix.exs
+++ api/mix.exs
@@ -38,7 +38,8 @@ defmodule Sling.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 2.5"}
     ]
   end
 
